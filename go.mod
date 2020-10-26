3550ecfbd5b3bdc0f7caec8491e972a2757739db
liuqin <1053544563@qq.com>
2020-10-26 21:36:05 +0800

    1.增加使用案例

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/go-ceres/example
+
+require (
+	github.com/go-ceres/go-ceres v0.0.0
+)
+replace (
+	github.com/go-ceres/go-ceres => ../go-ceres
+)
+
+go 1.14
---
+++ grpc/main.go
@@ -0,0 +1,28 @@
+//  Copyright 2020 Go-Ceres
+//  Author https://github.com/go-ceres/go-ceres
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package main
+
+import (
+	"github.com/go-ceres/go-ceres/engine"
+	"github.com/go-ceres/go-ceres/helper/log"
+)
+
+func main() {
+	app := engine.Default()
+	if err := app.Run(); err != nil {
+		log.Panic(err)
+	}
+}
